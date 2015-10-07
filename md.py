1424367ebef27b24fb91601cc6b45484dc268d63
Benjamin James Harrison-Sims <tehrainbowguy@gmail.com>
2015-10-07 01:38:41 +0100

    Cleanup script

--- md.py
+++ md.py
@@ -4,52 +4,51 @@
 # I would not recommend that you use this script, it's a horrible hack.
 # Don't come running to me if this breaks your site and eats your cat.
 
-from os import listdir
-import os, fnmatch
-from os.path import isfile, join
-import mistune
-import sys
+import os
+from os.path import join
 import time
 import logging
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
 
+import mistune
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler, FileModifiedEvent, FileCreatedEvent, FileDeletedEvent
 
 mypath = "md"
-def locate(pattern, root=os.curdir):
-    '''Locate all files matching supplied filename pattern in and below
-    supplied root directory.'''
-    for path, dirs, files in os.walk(os.path.abspath(root)):
-        for filename in fnmatch.filter(files, pattern):
-            yield os.path.join(path, filename) 
-    
 
-def process_files():
-    template = ""
+
+def process_file(file):
+    if file[len(file) - 3:len(file)] != ".md":
+        return
     with open("template.html", 'r') as r:
         template = r.read()
+    name = file.replace("md/", "pages/")
+    htmlname = name.replace(".md", ".html")
+    ospath = os.path.split(join(os.getcwd(), htmlname))[0]
+    if not os.path.exists(ospath):
+        os.makedirs(ospath)
+    with open(file, 'r') as r:
+        raw = r.read()
+        split = raw.split("~#~\n")
+        if len(split) == 1:
+            logging.info("New file without header detected, not processing")
+            return
+        props = dict(item.split("=") for item in split[0].split("\n"))
+        mark = mistune.markdown(split[1])
+        with open(htmlname, 'w') as w:
+            logging.info("Writing to {file}".format(file=htmlname))
+            w.write(template.replace("{TITLE}", props["TITLE"].replace("\"", "")).replace("{BODY}", mark))
 
-    cwd = join(os.getcwd(), mypath) + os.sep
-    for x in locate("*.md", mypath):
-        logging.info(x)
-        name = x.replace(cwd, "")
-        htmlname = join("pages", name.replace(".md", ".html"))
-        ospath = os.path.split(join(os.getcwd(), htmlname))[0]
-        if not os.path.exists(ospath):
-            os.makedirs(ospath)
-        with open(x, 'r') as r:
-            raw = r.read()
-            split = raw.split("~#~\n")
-            props = dict(item.split("=") for item in split[0].split("\n"))
-            mark = mistune.markdown(split[1])
-            with open(htmlname, 'w') as w:
-                logging.info("Writing to {file}".format(file=htmlname))
-                w.write(template.replace("{TITLE}", props["TITLE"].replace("\"", "")).replace("{BODY}", mark))
 
 class handler(FileSystemEventHandler):
     def on_any_event(this, event):
-        print(event)
-        process_files()
+        if type(event) is FileModifiedEvent or type(event) is FileCreatedEvent:
+            logging.info("File created or changed! Path: {path}".format(path=event.src_path))
+            process_file(event.src_path)
+        elif type(event) is FileDeletedEvent:
+            logging.info("File deleted! Path: {path}".format(path=event.src_path))
+            if os.path.exists(event.src_path):
+                os.remove(event.src_path.replace("/md/", "/pages/"))
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
